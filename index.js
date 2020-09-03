a38229dc43b69387f1d976cded2c424681780ae7
DmitriiMiasnikov <51396910+DmitriiMiasnikov@users.noreply.github.com>
2020-09-04 00:50:42 +0300

    create node repo

---
+++ index.js
@@ -0,0 +1,6 @@
+const userObj = require('./user')
+
+console.log(userObj.user);
+console.log(userObj.sayHello())
+console.log('hello', __dirname);
+console.log('hello', __filename);
---
+++ user.js
@@ -0,0 +1,14 @@
+const user = {
+    name: 'dmitrii',
+    age: 20,
+}
+const user2 = {
+    name: 'alex',
+}
+
+module.exports = {
+    user: user,
+    sayHello() {
+        console.log('hello')
+    }
+};
