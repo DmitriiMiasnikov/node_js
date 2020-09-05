6f90669946a21619bcb6933e3055e4d67aa643c5
DmitriiMiasnikov <51396910+DmitriiMiasnikov@users.noreply.github.com>
2020-09-05 06:26:27 +0300

    get post html

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-    console.log(req.url)
-    res.write('<h1>hello from node</h1>')
-    res.end('<div style="width: 100px; height: 100px; background-color: red"></div>')
+    if (req.method === "GET") {
+        res.writeHead(200, {
+            'Content-Type': 'text/html'
+        })
+        res.end(`
+        <h1>Form</h1>
+        <form method="post" action="/">
+        <input name="title" type="text" />
+        <button type="submit"> Send </button>
+        </form>
+        `)
+    } else if (req.method === 'POST') {
+        const body = []
+        res.writeHead(200, {
+            'Content-Type': 'text/html; charset=utf-8'
+        })
+        req.on('data', data => {
+            body.push(Buffer.from(data))
+        })
+        req.on('end', () => {
+           const message = body.toString().split('=')[1]
+           res.end(`
+           <h1>мое сообщение: ${message}</h1>
+       `)
+        })
+    }
 })
 server.listen(3000, () => {
     console.log('server has started')
